40430e2348fea05eb69b0f378d2f80af271100ab
Siyahul Haq <siyahul.haq@upayments.com>
2026-08-31 17:55:06 +0530

    chore: untrack .next build directory and update .gitignore

--- examples/nextjs/.next/static/chunks/app/layout-56c8569d262c434d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4125:function(n,e,u){Promise.resolve().then(u.t.bind(u,3366,23))},3366:function(){}},function(n){n.O(0,[709,285,749,744],function(){return n(n.s=4125)}),_N_E=n.O()}]);
